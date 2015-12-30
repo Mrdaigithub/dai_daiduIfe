45a35a97a84697585158bc3ccd39a956815d141f
Mrdaigithub <346139312@qq.com>
2015-12-30 22:16:12 +0800

    add index.html

--- dai-baiduife/2015_spring/task/task0003/source/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    document.addEventListener('click', function (event) {
-        let i=0;
-        for (let i=0;i<100;i++){
-            console.log(i);
-        }
-        alert('hello world!');
-    },false)
-}
